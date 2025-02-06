428a96feba03b863e52e919663393e86d8da6260
artosepyan <artyomosepyan@mail.ru>
2025-02-06 11:29:19 +0300

    Created index.phtml, urls.phtml

--- public/index.php
+++ public/index.php
@@ -3,11 +3,24 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
-$app = AppFactory::create();
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
+    $params = ["currentRoute" => "home"];
+    return $this->get('renderer')->render($response, "index.phtml", $params);
 });
+
+$app->get('/urls', function ($request, $response) {
+    $params = ["currentRoute" => "urls"];
+    return $this->get('renderer')->render($response, "urls.phtml", $params);
+});
+
 $app->run();
